624f329b117003612c57e7aabe1fe98ea9ee20e0
devolay <dawid.stachowiak@o2.pl>
2024-04-17 19:14:07 +0200

    add: added reading rgb data and visualization of the dataset sample

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -3,14 +3,14 @@ import numpy as np
 import torch
 
 from pathlib import Path
-from src.data.utils import depth_read
+from src.data.utils import read_calib_file, read_depth, read_rgb
 from torch.utils.data import Dataset
 from PIL import Image
 
 raw_data_dir = Path(__file__).resolve().parents[2] / "data" / "raw"
 
 
-class LIDARDataset(Dataset):
+class KittiDataset(Dataset):
     def __init__(self, root_dir=raw_data_dir, train=True, transform=None):
         """
         Args:
@@ -21,22 +21,28 @@ class LIDARDataset(Dataset):
         self.root_dir = root_dir
         self.train = train
         self.transform = transform
-        self.subset = 'train' if self.train else 'val'
-        self.data_list = self._load_image_paths_simple()
+        self.mode = 'train' if self.train else 'val'
+        self.data_list = self._load_data()
 
-    def _load_image_paths_simple(self):
+    def _load_data(self):
         """Load the file paths of images for LIDAR data from left and right cameras."""
         paths = []
-        gt_path = os.path.join(self.root_dir, 'data_depth_annotated', self.subset)
-        sparse_path = os.path.join(self.root_dir, 'data_depth_velodyne', self.subset)
+        gt_path = os.path.join(self.root_dir, 'data_depth_annotated', self.mode)
+        sparse_path = os.path.join(self.root_dir, 'data_depth_velodyne', self.mode)
+        raw_path = os.path.join(self.root_dir, "raw_kitti_data")
         for sequence_dir in os.listdir(gt_path):
+            date = '_'.join(sequence_dir.split('_')[:3])
             gt_seq = os.path.join(gt_path, sequence_dir, "proj_depth", "groundtruth")
             sparse_seq = os.path.join(sparse_path, sequence_dir, "proj_depth", "velodyne_raw")
+            raw_seq = os.path.join(raw_path, date, sequence_dir)
             for camera in ['image_02', 'image_03']:
                 images = [
                     {
                         "sparse": os.path.join(sparse_seq, camera, image.name),
-                        "groundtruth": os.path.join(gt_seq, camera, image.name)
+                        "gt": os.path.join(gt_seq, camera, image.name),
+                        "rgb": os.path.join(raw_seq, camera, "data", image.name),
+                        'calibration': os.path.join(raw_path, date, "calib_cam_to_cam.txt")
+
                     }
                     for image in Path(os.path.join(gt_seq, camera)).rglob('*.png')]
                 paths.extend(images)
@@ -46,7 +52,28 @@ class LIDARDataset(Dataset):
         return len(self.data_list)
 
     def __getitem__(self, idx):
-        images = self.data_list[idx]
-        sparse_depth_img = depth_read(images['sparse'])
-        gt_depth_img = depth_read(images['groundtruth'])
-        return sparse_depth_img, gt_depth_img
+        data = self.data_list[idx]
+        sparse = read_depth(data['sparse'])
+        gt = read_depth(data['gt'])
+        rgb = read_rgb(data['rgb'])
+
+        if self.mode in ['train', 'val']:
+            calib = read_calib_file(data['calibration'])
+            if 'image_02' in data['rgb']:
+                K_cam = np.reshape(calib['P_rect_02'], (3, 4))
+            elif 'image_03' in data['rgb']:
+                K_cam = np.reshape(calib['P_rect_03'], (3, 4))
+            K = [K_cam[0, 0], K_cam[1, 1], K_cam[0, 2], K_cam[1, 2]]
+        else:
+            f_calib = open(data['calibration'], 'r')
+            K_cam = f_calib.readline().split(' ')
+            f_calib.close()
+            K = [float(K_cam[0]), float(K_cam[4]), float(K_cam[2]), float(K_cam[5])]
+
+        w1, h1, _ = rgb.shape
+        w2, h2 = sparse.shape
+        w3, h3 = gt.shape
+
+        assert w1 == w2 and w1 == w3 and h1 == h2 and h1 == h3
+
+        return rgb, sparse, gt, K
--- src/data/utils.py
+++ src/data/utils.py
@@ -1,12 +1,36 @@
 from PIL import Image
 import numpy as np
+import os
 
-def depth_read(filename: str):
-    # loads depth map D from png file
-    # and returns it as a numpy array,
-    depth_png = np.array(Image.open(filename), dtype=int)
-    # make sure we have a proper 16bit depth map here.. not 8bit!
-    assert(np.max(depth_png) > 255)
-    depth = depth_png.astype(float) / 256.
-    depth[depth_png == 0] = -1.
-    return depth
+
+def read_rgb(file_name):
+    """Loads an RGB image from a file as a numpy array."""
+    assert os.path.exists(file_name), "file not found: {}".format(file_name)
+    image_rgb = np.array(Image.open(file_name))
+    image_rgb = image_rgb.astype(np.float32) / 255.0
+    return image_rgb
+
+def read_depth(file_name):
+    """Loads depth map from 16 bits png file as a numpy array"""
+    assert os.path.exists(file_name), "file not found: {}".format(file_name)
+    image_depth = np.array(Image.open(file_name))
+
+    # Consider empty depth
+    assert (np.max(image_depth) == 0) or (np.max(image_depth) > 255), \
+        "np.max(depth_png)={}, path={}".format(np.max(image_depth), file_name)
+
+    image_depth = image_depth.astype(np.float32) / 255.0
+    return image_depth
+
+
+def read_calib_file(filepath):
+    """Read in a calibration file and parse into a dictionary."""
+    data = {}
+    with open(filepath, 'r') as f:
+        for line in f.readlines():
+            key, value = line.split(':', 1)
+            try:
+                data[key] = np.array([float(x) for x in value.split()])
+            except ValueError:
+                pass
+    return data

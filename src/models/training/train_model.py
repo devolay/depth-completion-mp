8cccb31f095915ca915dc237baf288dac3132181
devolay <dawid.stachowiak@o2.pl>
2024-05-09 21:12:49 +0200

    add: updated requirements and training

--- src/models/training/train_model.py
+++ src/models/training/train_model.py
@@ -23,9 +23,9 @@ def train_model(config: TrainingConfig):
     )
 
     checkpoint_callback = ModelCheckpoint(
-        monitor='val_loss',
+        monitor='valid_rmse',
         dirpath=config.output_path,
-        filename='{epoch}-{val_loss:.2f}'
+        filename='{epoch}-{valid_rmse:.2f}'
     )
     
     training_dataset = KittiDataset(root_dir=config.dataset_path, load_raw=False)
